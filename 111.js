dbc9817c06a881fd3df6b9eb67b056a3aca47752
maciabe <37798196+maciabe@users.noreply.github.com>
2018-03-26 22:56:16 +0200

    Add files via upload

--- 111.js
+++ 111.js
@@ -39,7 +39,7 @@ for (num = 1; num < 20; num++){
     }
 }*/
 
-var links = document.getElementsByTagName('a');
+var links = document.getElementsByTagName('div');
 
 for (i = 1; i < links.length; i++)
 {
@@ -47,7 +47,7 @@ for (i = 1; i < links.length; i++)
         continue;
     }
     console.log(' loop number - ' + i);
-    if(i === 14){
+    if(i === 4){
         break;
 
     }
@@ -59,7 +59,7 @@ function NewAverage (a,b,c) {
 }
 NewAverage(4,7,11);
 
-//my own average
+/*my own average
 
 function myAverage (a,b,c,d){
 
@@ -70,8 +70,6 @@ function myAverage (a,b,c,d){
 }
 var theResult = myAverage(2,4,6,8);
 console.log(' once again, the average is : ' + theResult);
-
-
 // again
 
 function theAverage(a,b){
@@ -83,4 +81,76 @@ var RealAverage = theAverage(-3,5);
 console.log('the real average is : ' + RealAverage);
 
 document.write(typeof (RealAverage) );
-console.log('the type of this element is : ' + typeof (RealAverage) );
+console.log('the type of this element is : ' + typeof (RealAverage) );*/
+
+function multiplicationFam (a,b){
+    var multi = (a * b);
+    console.log('the result of the multiplication\'s of ' + a + ' and ' + b + ' is : ' + multi);
+    return(multi);
+}
+
+var newResult = multiplicationFam(100, 100);
+var PI = 3.14;
+console.log( "the result plus Pi is .. " + newResult + PI);
+
+
+var slicing = "i like ike";
+var newslice = slicing.slice(2,7,9,11);
+console.log(newslice);
+
+var splitting = "one, two, three, four, five";
+var NewSplit = splitting.split(',');
+console.log(NewSplit);
+
+var MyArray = [12,'pip',7,'lolo',false];
+console.log(MyArray);
+
+//objects plus METHODS
+var myArray2 = new Array();
+myArray2[0] = 20;
+myArray2[1] = 10;
+myArray2[2] = 34;
+
+myArray2.newfunction = function(){
+    document.write('sorted array : ' + myArray2);
+};
+myArray2.newfunction();
+
+var newDrive = {
+
+    maxSpeed: 50,
+    driver: "MyAss",
+    distance: 700,
+    mileage : function(time, speed){
+        console.log(time * speed);
+    }
+
+
+};
+console.log(newDrive.driver);
+newDrive.mileage(2,70);
+
+//age = prompt('whats your age ?');
+//console.log(age);
+
+var qq = "hello"
+if (!isNaN(qq)){
+    console.log('not a number');
+
+}else{console.log(qq);}
+
+
+var amount = 33;
+amount = amount * 2;
+console.log(amount);
+amount = " $$ " + String(amount);
+console.log(amount);
+
+// price
+var Price = 999.99;
+var Tax = 0.08;
+Price = Price + (Price * Tax)
+console.log(Price);
+console.log(Price.floor);
+console.log(typeof Price);
+
